21ef9b78e8c506da43e612f6097edd1315f8f47c
takkin <42467940+takkin-takilog@users.noreply.github.com>
2019-04-26 21:02:14 +0900

    [change] oanda_sample.py ⇒ pricing_stream.py

--- fx/pricing_stream.py
+++ fx/pricing_stream.py
@@ -1,44 +1,32 @@
 # ==============================================================================
-# brief        python OANDA サンプルコード
+# brief        OANDA Pricing Stream
 #
 # author       たっきん
 #
-# pip install oandapyV20
+# 事前準備 :
+#     oandapyV20のインストール (pip install oandapyV20)
 # ==============================================================================
 
 # ライブラリのインポート
 import json
-
 from oandapyV20 import API
 from oandapyV20.endpoints.pricing import PricingStream
 from oandapyV20.exceptions import V20Error
-
+from fx import oanda_common as oc
 from fx import your_account as ya
 
 
-class OANDA_ENV(object):
-    """ OANDA_ENV - OANDA環境の定義クラス。
-    パラメータ
-    ----------
-    PRACTICE : デモ口座
-    LIVE     : 本講座
-    """
-    PRACTICE = "practice"
-    LIVE = "live"
+api = API(access_token=ya.access_token, environment=oc.OANDA_ENV.PRACTICE)
 
-
-api = API(access_token=ya.access_token, environment=OANDA_ENV.PRACTICE)
-
-instruments = "USD_JPY"
-
-params = {"instruments": instruments}
+params = {"instruments": "USD_JPY"}
 ps = PricingStream(ya.account_number, params)
 
 try:
     for rsp in api.request(ps):
-        print("=================")
+        print("■リストの取得")
         print(json.dumps(rsp, indent=2))
 
+        print("■bidsのみ抽出：")
         if "bids" in rsp.keys():
             print(rsp["bids"][0]["price"])
 
