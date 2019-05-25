cc1cb98f8f575b8c69237ebcae69ab6e66bc5e3a
takkin <42467940+takkin-takilog@users.noreply.github.com>
2019-05-26 03:32:32 +0900

    [update]

--- datetime_test.py
+++ datetime_test.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta, timezone
+import pandas as pd
+from comtypes.npsupport import numpy as np
 
 DT_FMT = "%Y-%m-%dT%H:%M:%S"
 
@@ -8,12 +10,39 @@ JST = timezone(timedelta(hours=+9), 'JST')
 tdjp = datetime(year=2019, month=5, day=21, hour=13, minute=0, second=0)
 print("タイムゾーン：東京指定")
 print("{}   {}" .format(tdjp, tdjp.timestamp()))
+print(tdjp.tzinfo)
 
 td = datetime(year=2019, month=5, day=21, hour=13, minute=0, second=0)
 print("タイムゾーン：指定なし")
 print("{}   {}" .format(td, td.timestamp()))
-bb = td
+print(td.tzinfo)
 
 
+print("========== タイムスタンプ ==========")
+
+tsjp = pd.Timestamp('2018-01-01 12:00:00', tz='Asia/Tokyo')
+tsjpts = tsjp.timestamp()
+tsjpdt = tsjp.to_pydatetime()
+print("タイムゾーン：東京指定")
+print("{} / {} / {}" .format(tsjp, tsjpts, datetime.fromtimestamp(tsjpts)))
+print("{} / {}" .format(tsjpdt, datetime.fromtimestamp(tsjpts)))
+print("{} / {}" .format(tsjpdt, datetime.utcfromtimestamp(tsjpts)))
+
+ts = pd.Timestamp('2018-01-01 12:00:00')
+tsts = ts.timestamp()
+print("タイムゾーン：指定なし")
+print("{} / {} / {}" .format(ts, tsts, datetime.fromtimestamp(tsts)))
+
+
+print("========== pandas ==========")
+# dti = pd.date_range(start='1/1/2018', periods=5, tz='Asia/Tokyo')
+dti = pd.date_range(start='1/1/2018', periods=5)
+print(dti)
+uti = dti.tz_localize('Asia/Tokyo').astype(np.int64) // 10**9
+print(uti)
+print("========== pandas2 ==========")
+
+df = pd.DataFrame({"ts": dti, "uti":uti})
+print(df)
 
 
