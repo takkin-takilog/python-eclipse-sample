4e2d47d96fc260be9f83e787898cd692545b5d89
takkin <takkin.takilog@gmail.com>
2020-02-23 17:58:13 +0900

    [update]

--- fx/orders/order_cancel_test.py
+++ fx/orders/order_cancel_test.py
@@ -18,21 +18,24 @@ from fx import your_account as ya
 # 保留中となるオーダーを作成しておくこと
 # -----------------------------
 
-order_id = 0  # 作成したオーダーIDを指定
+order_id = 192  # 作成したオーダーIDを指定
 
 api = API(access_token=ya.access_token, environment=oc.OandaEnv.PRACTICE)
 
 # 保留中の注文情報を確認
+print("①保留中の注文情報を確認（キャンセル前）")
 ep = OrderDetails(accountID=ya.account_number, orderID=order_id)
 rsp = api.request(ep)
 print(json.dumps(rsp, indent=2))
 
 # 保留中の注文をキャンセルする
+print("②保留中の注文キャンセル")
 ep = OrderCancel(accountID=ya.account_number, orderID=order_id)
 rsp = api.request(ep)
 print(json.dumps(rsp, indent=2))
 
 # 保留中の注文情報を再度確認
+print("③保留中の注文情報を確認（キャンセル後）")
 ep = OrderDetails(accountID=ya.account_number, orderID=order_id)
 rsp = api.request(ep)
 print(json.dumps(rsp, indent=2))
--- fx/orders/order_replace_test.py
+++ fx/orders/order_replace_test.py
@@ -18,7 +18,7 @@ from fx import your_account as ya
 # 保留中となるオーダーを作成しておくこと
 # -----------------------------
 
-order_id = 103  # 作成したオーダーIDを指定
+order_id = 174  # 作成したオーダーIDを指定
 
 api = API(access_token=ya.access_token, environment=oc.OandaEnv.PRACTICE)
 
@@ -34,10 +34,8 @@ data = {
         "timeInForce": "GTC",
         "instrument": "USD_JPY",
         "type": "LIMIT",
-        "units": 10000,
-        "type": "LIMIT",
+        "units": 500,
         "positionFill": "DEFAULT",
-        "tradeID": "103",
     },
 }
 ep = OrderReplace(accountID=ya.account_number, orderID=order_id,
