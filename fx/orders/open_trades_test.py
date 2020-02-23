4429a4cba988b1856a2387ce8d2eeb715e83b4aa
takkin <takkin.takilog@gmail.com>
2020-02-24 00:12:31 +0900

    [update]

--- fx/orders/open_trades_test.py
+++ fx/orders/open_trades_test.py
@@ -1,6 +1,6 @@
 # ==============================================================================
-# brief        OANDA OpenPositions API Test
-#              OANDA オープンポジション取得APIテスト
+# brief        OANDA OpenTrades API Test
+#              OANDA オープントレード取得APIテスト
 #
 # author       たっきん
 #
@@ -9,7 +9,7 @@
 # ==============================================================================
 
 import json
-from oandapyV20.endpoints.positions import OpenPositions
+from oandapyV20.endpoints.trades import OpenTrades
 from oandapyV20 import API
 from fx import oanda_common as oc
 from fx import your_account as ya
@@ -20,7 +20,7 @@ from fx import your_account as ya
 
 api = API(access_token=ya.access_token, environment=oc.OandaEnv.PRACTICE)
 
-# 取引中の全ポジション情報を取得する
-ep = OpenPositions(accountID=ya.account_number)
+# 取引中の全トレード情報を取得する
+ep = OpenTrades(accountID=ya.account_number)
 rsp = api.request(ep)
 print(json.dumps(rsp, indent=2))
