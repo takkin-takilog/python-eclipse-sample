df8a46306d502728d2e1c3f13e3bb4345c96979a
takkin <takkin.takilog@gmail.com>
2020-09-16 02:53:50 +0900

    [add] pricing_info.py

--- fx/pricing_info.py
+++ fx/pricing_info.py
@@ -1,6 +1,6 @@
 # ==============================================================================
 # brief        OANDA Pricing Stream
-#              リアルタイムで為替レート値を取得する。（ストリーム形式）
+#              リアルタイムで為替レート値を取得する。
 #
 # author       たっきん
 #
@@ -8,28 +8,25 @@
 #     oandapyV20のインストール (pip install oandapyV20)
 # ==============================================================================
 
+import time
 import json
 from oandapyV20 import API
-from oandapyV20.endpoints.pricing import PricingStream
+from oandapyV20.endpoints.pricing import PricingInfo
 from oandapyV20.exceptions import V20Error
 from fx import oanda_common as oc
 from fx import your_account as ya
 
-
 api = API(access_token=ya.access_token, environment=oc.OandaEnv.PRACTICE)
 
 params = {"instruments": "USD_JPY,EUR_JPY,EUR_USD"}
-ps = PricingStream(ya.account_number, params)
+pi = PricingInfo(ya.account_number, params)
 
 try:
-    for rsp in api.request(ps):
+    while True:
+        rsp = api.request(pi)
         print("■リストの取得")
         print(json.dumps(rsp, indent=2))
-
-        print("■bidsのみ抽出：")
-        if "bids" in rsp.keys():
-            print(rsp["bids"][0]["price"])
-
+        time.sleep(1)
 
 except V20Error as e:
     print("Error: {}".format(e))
