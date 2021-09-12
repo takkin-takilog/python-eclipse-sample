d9fa13c86b609e3f606a13e8233e91c35b06ea23
takkin <takkin.takilog@gmail.com>
2021-09-13 02:05:36 +0900

    [update]

--- pandas_test/pandas_sample_004.py
+++ pandas_test/pandas_sample_004.py
@@ -2,16 +2,16 @@ import datetime as dt
 import pandas as pd
 
 data_mat = [
-    [dt.datetime(2021, 1, 15, 13, 1), 100, 200, 300],
-    [dt.datetime(2021, 1, 15, 13, 2), 101, 201, 301],
-    [dt.datetime(2021, 1, 15, 13, 3), 102, 202, 302],
-    [dt.datetime(2021, 1, 15, 13, 4), 103, 203, 303],
-    [dt.datetime(2021, 1, 15, 13, 5), 104, 204, 304],
-    [dt.datetime(2021, 1, 15, 13, 6), 105, 205, 305],
-    [dt.datetime(2021, 1, 15, 13, 7), 106, 206, 306],
-    [dt.datetime(2021, 1, 15, 13, 8), 107, 207, 307],
-    [dt.datetime(2021, 1, 15, 13, 9), 108, 208, 308],
-    [dt.datetime(2021, 1, 15, 13, 10), 109, 209, 309],
+    [dt.datetime(2021, 1, 15, 13, 1), 100, 200, 300, True],
+    [dt.datetime(2021, 1, 15, 13, 2), 101, 201, 301, True],
+    [dt.datetime(2021, 1, 15, 13, 3), 102, 202, 302, True],
+    [dt.datetime(2021, 1, 15, 13, 4), 103, 203, 303, True],
+    [dt.datetime(2021, 1, 15, 13, 5), 104, 204, 304, True],
+    [dt.datetime(2021, 1, 15, 13, 6), 105, 205, 305, True],
+    [dt.datetime(2021, 1, 15, 13, 7), 106, 206, 306, True],
+    [dt.datetime(2021, 1, 15, 13, 8), 107, 207, 307, True],
+    [dt.datetime(2021, 1, 15, 13, 9), 108, 208, 308, False],
+    [dt.datetime(2021, 1, 15, 13, 10), 109, 209, 309, False],
 ]
 
 
@@ -19,9 +19,10 @@ COL_DATETIME = "DateTime"
 COL_FIRST = "First"
 COL_SECOND = "Second"
 COL_THARD = "Thard"
+COL_BOOL = "Bool"
 
 df_org = pd.DataFrame(data_mat,
-                      columns=[COL_DATETIME, COL_FIRST, COL_SECOND, COL_THARD])
+                      columns=[COL_DATETIME, COL_FIRST, COL_SECOND, COL_THARD, COL_BOOL])
 df = df_org.set_index(COL_DATETIME)
 
 print("{:-^60}".format(" DataFrame初期化 "))
@@ -29,13 +30,13 @@ print(df)
 
 print("{:-^60}".format(" 行指定での値一括変更① "))
 target_idx = dt.datetime(2021, 1, 15, 13, 6)
-df.loc[target_idx] = [406, 506, 106]
+df.loc[target_idx] = [406, 506, 106, True]
 print(df)
 
 print("{:-^60}".format(" 行指定での値一括変更② "))
 # 指定したインデックスが存在しなければ、最終行に追加される。
 target_idx = dt.datetime(2021, 1, 15, 10, 1)
-df.loc[target_idx] = [406, 506, 106]
+df.loc[target_idx] = [406, 506, 106, True]
 print(df)
 
 print("{:-^60}".format(" インデックスを昇順にソート "))
@@ -53,5 +54,9 @@ print(df)
 df_drop = df.drop(df[:2].index)
 print(df_drop)
 
+print("{:-^60}".format(" True/Falseのみ抽出 "))
+df_true = df[df[COL_BOOL]]
+df_false = df[~df[COL_BOOL]]
 print(df)
-print(df.iloc[2:])
+print(df_true)
+print(df_false)
